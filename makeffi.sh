5c746f26bb7c4348ee86636836a475f5d494416b
ppc <hjiayz@gmail.com>
2021-01-19 00:39:26 +0800

    Update makeffi.sh

--- makeffi.sh
+++ makeffi.sh
@@ -3,19 +3,23 @@ export Host=linux-x86_64
 export NDK=$MYPATH/android-ndk-r22
 export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$Host
 export TARGET=$1
+if test -z "$2"
+then
+  export CC_TARGET=$1
+else  
+  export CC_TARGET=$2
+fi
+export CLANGTARGET=
 export API=21
 export AR=$TOOLCHAIN/bin/$TARGET-ar
 export AS=$TOOLCHAIN/bin/$TARGET-as
-export CC=$TOOLCHAIN/bin/$TARGET$API-clang
-export CXX=$TOOLCHAIN/bin/$TARGET$API-clang++
+export CC=$TOOLCHAIN/bin/$CC_TARGET$API-clang
+export CXX=$TOOLCHAIN/bin/$CC_TARGET$API-clang++
 export LD=$TOOLCHAIN/bin/$TARGET-ld
 export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
 export STRIP=$TOOLCHAIN/bin/$TARGET-strip
 export READELF=$TOOLCHAIN/bin/$TARGET-readelf
 cd libffi-3.3
-./configure --host=$TARGET
+./configure --host=$CC_TARGET
 make
-export CFLAGS=$CFLAGS -I$MYPATH/libffi-3.3/$TARGET/include
-export LDFLAGS=$LDFLAGS -L$MYPATH/libffi-3.3/$TARGET -static
-export LINKFORSHARED=""
 cd ..
