1c2767343a307696f0eafc46696af2b0c14862f9
ppc <hjiayz@gmail.com>
2021-01-18 18:11:32 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+
+wget -q https://dl.google.com/android/repository/android-ndk-r21d-linux-x86_64.zip
+unzip android-ndk-r21d-linux-x86_64.zip > /dev/null
+wget -q ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
+tar zxvf libffi-3.3.tar.gz > /dev/null
+wget -q https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
+tar -xvJf Python-3.9.1.tar.xz > /dev/null
+cd Python-3.9.1
+./configure
+make
+make install
+make clean
+cd ..
