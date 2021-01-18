6987d2d5d6f826dbf517a4aa5f2a8a27768e901f
ppc <hjiayz@gmail.com>
2021-01-18 19:16:12 +0800

    Update makepy.sh

--- makepy.sh
+++ makepy.sh
@@ -12,9 +12,9 @@ export LD=$TOOLCHAIN/bin/$TARGET-ld
 export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
 export STRIP=$TOOLCHAIN/bin/$TARGET-strip
 export READELF=$TOOLCHAIN/bin/$TARGET-readelf
-export CFLAGS= -I$MYPATH/libffi-3.3/$TARGET/include
-export LDFLAGS= -L$MYPATH/libffi-3.3/$TARGET -static
+export CFLAGS=-I$MYPATH/libffi-3.3/$TARGET/include
+export LDFLAGS=-L$MYPATH/libffi-3.3/$TARGET -static
 export LINKFORSHARED=""
 cd Python-3.9.1
-./configure --host=$TARGET --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no
+./configure ./configure --host=x86_64-linux -build=$TARGET --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no
 make
