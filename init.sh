1f36ad456336c2fc5eafbd006dbfe1a4221197fe
ppc <hjiayz@gmail.com>
2021-01-19 00:12:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 
 wget -q https://dl.google.com/android/repository/android-ndk-r22-linux-x86_64.zip
-unzip android-ndk-r21d-linux-x86_64.zip > /dev/null
+unzip android-ndk-r22-linux-x86_64.zip > /dev/null
 wget -q ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
 tar zxvf libffi-3.3.tar.gz > /dev/null
 wget -q https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
