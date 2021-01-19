17c14ee70aa6bf98b9601ec1d8c4dbcdc164d24d
ppc <hjiayz@gmail.com>
2021-01-19 17:58:28 +0800

    Update makessl.sh

--- makessl.sh
+++ makessl.sh
@@ -5,6 +5,7 @@ export ANDROID_NDK_ROOT=$MYPATH/android-ndk-r22
 export TOOLCHAIN=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$Host
 export TARGET=$1
 echo $ANDROID_NDK_ROOT
+ls $ANDROID_NDK_ROOT
 cd openssl-1.1.1i
 PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
 ./Configure $TARGET -D__ANDROID_API__=29
