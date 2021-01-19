f0adb9a8f606b4db164bc5ea54a4fbb491816e4f
ppc <hjiayz@gmail.com>
2021-01-19 17:04:58 +0800

    Create makessl.sh

---
+++ makessl.sh
@@ -0,0 +1,9 @@
+export MYPATH=$(dirname $(readlink -f "$0"))
+export NDK=$MYPATH/android-ndk-r22
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$Host
+export TARGET=$1
+cd openssl-1.1.1i
+PATH=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin:$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
+./Configure $TARGET -D__ANDROID_API__=29
+make
+cd ..
