99cbbb310d3bbfd4b13d8eb879dab314f4bc5165
ppc <hjiayz@gmail.com>
2021-01-19 22:38:37 +0800

    Update makessl.sh

--- makessl.sh
+++ makessl.sh
@@ -1,6 +1,8 @@
 export MYPATH=$(dirname $(readlink -f "$0"))
 export NDK=$MYPATH/android-ndk-r22
 export ANDROID_NDK_ROOT=$MYPATH/android-ndk-r22
+export ANDROID_NDK_HOME=$MYPATH/android-ndk-r22
+export ANDROID_NDK=$MYPATH/android-ndk-r22
 export TOOLCHAIN=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$Host
 export TARGET=$1
 git clone https://github.com/hjiayz/openssl.git
