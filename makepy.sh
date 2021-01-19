d78dbeb5e6a3431565bff38e3f6ca469dfc030bb
网事如风 <w426405@gmail.com>
2021-01-19 10:34:23 +0800

    Update makepy.sh

--- makepy.sh
+++ makepy.sh
@@ -9,7 +9,7 @@ then
 else  
   export CC_TARGET=$2
 fi
-export CLANGTARGET=
+export CLANGTARGET=" "
 export API=21
 export AR=$TOOLCHAIN/bin/$TARGET-ar
 export AS=$TOOLCHAIN/bin/$TARGET-as
@@ -19,9 +19,9 @@ export LD=$TOOLCHAIN/bin/$TARGET-ld
 export RANLIB=$TOOLCHAIN/bin/$TARGET-ranlib
 export STRIP=$TOOLCHAIN/bin/$TARGET-strip
 export READELF=$TOOLCHAIN/bin/$TARGET-readelf
-export CFLAGS=-I$MYPATH/libffi-3.3/$TARGET/include -I$TOOLCHAIN/sysroot/usr/include
-export LDFLAGS=-L$MYPATH/libffi-3.3/$TARGET -static
-export LINKFORSHARED=""
+export CFLAGS="-I$MYPATH/libffi-3.3/$TARGET/include -I$TOOLCHAIN/sysroot/usr/include"
+export LDFLAGS="-L$MYPATH/libffi-3.3/$TARGET -static"
+export LINKFORSHARED=" "
 cd Python-3.9.1
 ./configure --host=x86_64-linux -build=$CC_TARGET --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no
 make
