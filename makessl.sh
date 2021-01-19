507164c46c3f1bb07090a6b39f7d9348a3a96dcb
ppc <hjiayz@gmail.com>
2021-01-19 22:52:44 +0800

    Update makessl.sh

--- makessl.sh
+++ makessl.sh
@@ -9,7 +9,7 @@ git clone https://github.com/hjiayz/openssl.git
 cd openssl
 git checkout igtd/android-ndk-r22
 export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
-./Configure $TARGET -D__ANDROID_API__=29
+./Configure $TARGET -D__ANDROID_API__=29 -static
 make
 ls
 cd ..
