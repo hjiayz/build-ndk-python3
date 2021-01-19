b53cc0e830bd352b377fa96caaca986e14ceb1c9
ppc <hjiayz@gmail.com>
2021-01-19 21:08:28 +0800

    Update makessl.sh

--- makessl.sh
+++ makessl.sh
@@ -1,12 +1,11 @@
-pwd
 export MYPATH=$(dirname $(readlink -f "$0"))
 export NDK=$MYPATH/android-ndk-r22
 export ANDROID_NDK_ROOT=$MYPATH/android-ndk-r22
 export TOOLCHAIN=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$Host
 export TARGET=$1
-echo $ANDROID_NDK_ROOT
-ls $ANDROID_NDK_ROOT
-cd openssl-3.0.0-alpha10
+git clone https://github.com/hjiayz/openssl.git
+cd openssl
+git checkout igtd/android-ndk-r22
 export PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$PATH
 ./Configure $TARGET -D__ANDROID_API__=29
 make
