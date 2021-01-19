73b6a7dc683b260017c9f257002421dcaef04cf8
ppc <hjiayz@gmail.com>
2021-01-19 21:03:04 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,6 @@ wget -q ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
 tar zxvf libffi-3.3.tar.gz > /dev/null
 wget -q https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
 tar -xvJf Python-3.9.1.tar.xz > /dev/null
-wget -q https://www.openssl.org/source/openssl-3.0.0-alpha10.tar.gz
-tar zxvf openssl-3.0.0-alpha10.tar.gz > /dev/null
 #cd Python-3.9.1
 #./configure
 #make
