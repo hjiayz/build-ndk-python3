6a85ca6c4585db9c7cfc44a768345361a20cb72a
ppc <hjiayz@gmail.com>
2021-01-20 13:11:42 +0800

    Update makepy.sh

--- makepy.sh
+++ makepy.sh
@@ -23,6 +23,8 @@ export CFLAGS="-I$MYPATH/libffi-3.3/$TARGET/include -fPIC"
 export LDFLAGS="-L$MYPATH/libffi-3.3/$TARGET" -static
 # export LINKFORSHARED=" "
 cd Python-3.9.1
-./configure --host=$CC_TARGET --target=$CC_TARGET --build=x86_64-linux  --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --with-openssl=$MYPATH/openssl-dir
+./configure --host=$CC_TARGET --target=$CC_TARGET --build=x86_64-linux  --disable-ipv6 ac_cv_file__dev_ptmx=no ac_cv_file__dev_ptc=no --with-openssl=$MYPATH/openssl-dir --prefix=$MYPATH/zip-dir/$TARGET
 make
+make install
+make clean
 cd ..
