a68869bf6e0708d8e501746623a2f0e4cbf59c6d
ppc <hjiayz@gmail.com>
2021-01-30 18:08:26 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,4 @@ wget -q ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz
 tar zxvf libffi-3.3.tar.gz > /dev/null
 wget -q https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
 tar -xvJf Python-3.9.1.tar.xz > /dev/null
-wget -q https://github.com/Reference-LAPACK/lapack/archive/v3.9.0.tar.gz
-tar -zxvf v3.9.0.tar.gz
+patch Python-3.9.1/setup.py python.patch
